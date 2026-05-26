72c251ccde5ada4b96e9dbd7bcf994f8aa481be9
Uklejamini357 <uklejamini@tutamail.com>
2026-05-26 19:13:24 +0200

    Fixed lua errors on startup on x86-64 branch Fixes lua errors for the function in InitPostEntity with arc9_phystweak enabled on the x86-64 branch.
    Running physenv.GetPerformanceSettings() immediately in InitPostEntity hook returns nil;
    And because there is no check for if the table is not nil, it creates a lua error when trying to get value physenvtbl.MaxVelocity as because the physenvtbl table is nil, which could potentially break other addons.
    
    To fix this issue, I added a timer with 0s, executing the same function. The only difference is that when the timer function is ran (After InitPostEntity), the physenv.GetPerformanceSettings() function returns a table and allows setting the MaxVelocity variable. This small change prevents causing any lua errors on startup with arc9_phystweak 1.

--- lua/arc9/common/sh_common.lua
+++ lua/arc9/common/sh_common.lua
@@ -468,11 +468,13 @@ end
 hook.Add("InitPostEntity", "ARC9_phystweak", function() -- stolen from tacrp
     if GetConVar("arc9_phystweak"):GetBool() then
         if !physenv or !physenv.GetPerformanceSettings then print("[ARC9] How the hell you don't have physenv???? wtf wrong with your game") return end
-        local v = physenv.GetPerformanceSettings().MaxVelocity or 10000
-        if v < 10000 then
-            physenv.SetPerformanceSettings({MaxVelocity = 10000})
-            print("[ARC9] Increasing MaxVelocity for projectiles to behave as intended! (" .. v .. "-> 10000)")
-            print("[ARC9] Disable this behavior with 'arc9_phystweak 0'.")
-        end
+        timer.Simple(0, function() -- Add timer because using physenv.GetPerformanceSettings() function immediately returns nil
+            local v = physenv.GetPerformanceSettings().MaxVelocity or 10000
+            if v < 10000 then
+                physenv.SetPerformanceSettings({MaxVelocity = 10000})
+                print("[ARC9] Increasing MaxVelocity for physics objects to make sure bullet projectiles behave as intended! (" .. v .. "-> 10000)")
+                print("[ARC9] Disable this behavior with 'arc9_phystweak 0'.")
+            end
+        end)
     end
 end)
